f67bf55280afe33bc2b7aeb2784503694c072f30
Allan Kildare <allankildare27@gmail.com>
2021-01-30 12:18:08 -0300

    + nodemon instalado e rota de atendimentos criada

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express()
 const port = 8080
 
 app.listen(port, () => console.log(`Servidor rodando na porta http://localhost:${port}`))
-app.get('/', (req, res) => res.send('<h1>Servidor rodando, tudo ok</h1>'))
+app.get('/atendimentos', (req, res) => res.send('<h1>Você está na rota de atendimentos</h1>'))

14d2b2e9d076251e9fe7ffdb993cafe1188780b2
Allan Kildare <allankildare27@gmail.com>
2021-01-30 12:52:18 -0300

    ~ body parser instalado e implementado na rota post

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,8 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('<h1>Você está olá na rota de atendimentos</h1>'))
-    app.post('/atendimentos', (req, res) => res.send('POST: Você está na rota de atendimentos'))
+    
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('POST: Você está na rota de atendimentos')}
+    )
 }

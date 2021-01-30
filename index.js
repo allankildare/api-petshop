538b5d3b20df0ad71c5734610d45c9d07705cdc8
Allan Kildare <allankildare27@gmail.com>
2021-01-30 19:05:45 -0300

    + tabela atendimentos (SQL)

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const customExpress = require("./config/customExpress")
 const port = 8080
 const conexao = require("./infra/conexao")
+const Tabelas = require("./infra/tabelas")
 
 conexao.connect((error) => {
   if (error) console.log(error);
   else {
     console.log("Conectado com sucesso!!!")
+    Tabelas.init(conexao)
     const app = customExpress()
     app.listen(port, () =>
       console.log(`Servidor rodando na porta http://localhost:${port}`)
